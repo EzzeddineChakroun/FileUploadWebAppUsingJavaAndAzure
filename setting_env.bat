ef1718f87775562e1eee81c89c1477ca1e0d8ab8
echakroun <echakroun@dom.tti>
2022-11-02 10:02:41 +0100

    setting env variables for our web app

---
+++ setting_env.bat
@@ -0,0 +1,7 @@
+export AZ_SUBSCRIPTION_ID=<your-azure-subscription-id>
+export AZ_RESOURCE_GROUP=<your-resource-group>
+export AZ_REGION=<choose-a-region>
+export AZ_APP_NAME=<your-unique-app-name>
+export AZ_PRICING_TIER=F1
+export AZ_STORAGE_CONNECTION_STRING=<your-azure-storage-connection-string>
+export AZ_STORAGE_CONTAINER_NAME=files
